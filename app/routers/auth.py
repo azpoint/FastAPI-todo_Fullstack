7c652072a8916a21dee1bb50b3adde37aafa7cef
change the name at local repo <changethemail@atlocal.repo>
2025-09-05 22:09:28 +0200

    passlib install

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,8 +1,55 @@
-from fastapi import APIRouter
+from typing import Annotated
+from fastapi import APIRouter, Depends
+from pydantic import BaseModel
+from app.database import SessionLocal
+from app.models import Users
+from passlib.context import CryptContext
+from sqlalchemy.orm import Session
+from starlette import status
+
 
 router = APIRouter()
 
+# Bcrypt config
+bcrypt_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
 
-@router.get("/auth/")
-async def get_user():
-    return {"user": "authenticated"}
+
+# DB connection
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
+
+
+db_dependency = Annotated[Session, Depends(get_db)]
+
+
+# Pydantic Validation model
+class CreateUserRequest(BaseModel):
+    username: str
+    email: str
+    first_name: str
+    last_name: str
+    password: str
+    role: str
+
+
+# Auth router endpoints
+# Create User endpoint
+@router.post("/auth/", status_code=status.HTTP_201_CREATED)
+async def create_user(db: db_dependency, create_user_request: CreateUserRequest):
+    # Build the data before putting to the DB
+    create_user_model = Users(
+        email=create_user_request.email,
+        username=create_user_request.username,
+        first_name=create_user_request.first_name,
+        last_name=create_user_request.last_name,
+        role=create_user_request.role,
+        hashed_password=bcrypt_context.hash(create_user_request.password),
+        is_active=True,
+    )
+
+    db.add(create_user_model)
+    db.commit()
--- app/routers/todos.py
+++ app/routers/todos.py
@@ -10,6 +10,7 @@ from pydantic import BaseModel, Field
 router = APIRouter()
 
 
+# DB connection
 def get_db():
     db = SessionLocal()
     try:
@@ -21,6 +22,7 @@ def get_db():
 db_dependency = Annotated[Session, Depends(get_db)]
 
 
+# Pydantic Validation model
 class TodoRequest(BaseModel):
     title: str = Field(min_length=3)
     description: str = Field(min_length=3, max_length=300)
@@ -28,23 +30,30 @@ class TodoRequest(BaseModel):
     complete: bool
 
 
+# Router todo endpoints
+# Get all todos
 @router.get("/", status_code=status.HTTP_200_OK)
 async def read_all(db: db_dependency):
     return db.query(Todos).all()
 
 
+# Get todo by id with path params
 @router.get("/todo/{todo_id}", status_code=status.HTTP_200_OK)
 async def read_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
     # DB request
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
+
     if todo_model is not None:
         return todo_model
+
     raise HTTPException(status_code=404, detail="Todo not found")
 
 
+# Create todo
 @router.post("/todo", status_code=status.HTTP_201_CREATED)
 async def create_todo(db: db_dependency, todo_request: TodoRequest):
+    # Validate data with Pydantic Model
     todo_model = Todos(**todo_request.model_dump())
 
     # Adding the data to the DB
@@ -52,30 +61,34 @@ async def create_todo(db: db_dependency, todo_request: TodoRequest):
     db.commit()
 
 
+# Update todo by ID with path params
 @router.put("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def update_todo(
     db: db_dependency, todo_request: TodoRequest, todo_id: int = Path(gt=0)
 ):
 
-    # Actual DB data request
+    # Fetch todo by ID from DB first before update.
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
 
     if todo_model is None:
         raise HTTPException(status_code=404, detail="Todo not found")
 
+    # Update data from DB with the request body of the endpoint.
     todo_model.title = todo_request.title
     todo_model.description = todo_request.description
     todo_model.priority = todo_request.priority
     todo_model.complete = todo_request.complete
 
+    # Add and commit changes to the DB
     db.add(todo_model)
     db.commit()
 
 
+# Delete todo by ID with path params
 @router.delete("/todo/{todo_id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_todo(db: db_dependency, todo_id: int = Path(gt=0)):
 
-    # Fetch DB data vefore delete
+    # Fetch DB data before delete
     todo_model = db.query(Todos).filter(Todos.id == todo_id).first()
 
     if todo_model is None:
