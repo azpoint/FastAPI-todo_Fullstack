aa4c3dffe6f01cf8924ab397f053a0df4f7010c0
change the name at local repo <changethemail@atlocal.repo>
2025-09-07 20:17:49 +0200

    users-router

--- app/routers/auth.py
+++ app/routers/auth.py
@@ -69,9 +69,11 @@ def authenticate_user(username: str, password: str, db):
 
 
 # JWT create/encode token
-def create_access_token(username: str, user_id: int, expires_delta: timedelta):
+def create_access_token(
+    username: str, user_id: int, role: str, expires_delta: timedelta
+):
 
-    encode = {"sub": username, "id": user_id}
+    encode = {"sub": username, "id": user_id, "role": role}
 
     expires = datetime.now(timezone.utc) + expires_delta
 
@@ -87,6 +89,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
 
         username: str = payload.get("sub")
         user_id: int = payload.get("id")
+        user_role: str = payload.get("role")
 
         if username is None or user_id is None:
             raise HTTPException(
@@ -94,7 +97,7 @@ async def get_current_user(token: Annotated[str, Depends(oauth2_bearer)]):
                 detail="Could not validate user",
             )
 
-        return {"username": username, "id": user_id}
+        return {"username": username, "id": user_id, "user_role": user_role}
 
     except JWTError:
         raise HTTPException(
@@ -133,6 +136,8 @@ async def login_for_access_token(
             detail="Could not validate user",
         )
 
-    token = create_access_token(user.username, user.id, timedelta(minutes=20))
+    token = create_access_token(
+        user.username, user.id, user.role, timedelta(minutes=20)
+    )
 
     return {"access_token": token, "token_type": "bearer"}
