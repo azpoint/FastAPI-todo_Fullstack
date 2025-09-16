fbe797e72fdcf5fe6acbf9353b276bcf7f48feca
change the name at local repo <changethemail@atlocal.repo>
2025-09-16 19:54:04 +0200

    static-setup

--- app/main.py
+++ app/main.py
@@ -1,12 +1,26 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from app.models import Base
 from app.database import engine
 from app.routers import auth, todos, admin, users
+from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 
 app = FastAPI()
 
 Base.metadata.create_all(bind=engine)
+
+# Set html template generator
+templates = Jinja2Templates(directory="templates")
+
+# Set static files server
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+#
+@app.get("/")
+def test(request: Request):
+    return templates.TemplateResponse("home.html", {"request": request})
 
 
 @app.get("/healthy")
